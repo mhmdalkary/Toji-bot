70928526df5d7aa10c1d0b6c445ef97dad43b580
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-04 20:04:36 +0200

    Update إزالة تقييد من قبلي (copy).js

--- scripts/cmds/إزالة تقييد من قبلي (copy).js
+++ scripts/cmds/إزالة تقييد من قبلي (copy).js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 const jimp = require("jimp");
-const fs = require("fs")
-
+const fs = require("fs");
 
 module.exports = {
     config: {
@@ -12,39 +11,48 @@ module.exports = {
         countDown: 5,
         role: 0,
         shortDescription: "ضرب",
-        longDescription: "",
-        category: "اضرب",
+        longDescription: "ضرب",
+        category: "صور",
         guide: ""
     },
 
-
-
     onStart: async function ({ message, event, args }) {
         const mention = Object.keys(event.mentions);
-        if (mention.length == 0) return message.reply("تاغ يلي بدك تضربه 😂");
-        else if (mention.length == 1) {
-            const one = event.senderID, two = mention[0];
-            bal(one, two).then(ptth => { message.reply({ body: "هههههههه", attachment: fs.createReadStream(ptth) }) })
+        if (mention.length === 0)
+            return message.reply("تاغ يلي بدك تضربه 😂");
+        else if (mention.length === 1) {
+            const الضارب = event.senderID;
+            const الضحية = mention[0];
+            تركيب(الضارب, الضحية).then(pth => {
+                message.reply({ body: "ههههههههه طاح فيها المسكين 💥", attachment: fs.createReadStream(pth) });
+            });
         } else {
-            const one = mention[1], two = mention[0];
-            bal(one, two).then(ptth => { message.reply({ body: "هههههه", attachment: fs.createReadStream(ptth) }) })
+            const الضارب = mention[1];
+            const الضحية = mention[0];
+            تركيب(الضارب, الضحية).then(pth => {
+                message.reply({ body: "هههههههههههه 🤣", attachment: fs.createReadStream(pth) });
+            });
         }
     }
-
-
 };
 
-async function bal(one, two) {
+async function تركيب(ضارب, ضحية) {
+    // تحميل صور البروفايل
+    const صورةالضارب = await jimp.read(`https://graph.facebook.com/${ضارب}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
+    const صورةالضحية = await jimp.read(`https://graph.facebook.com/${ضحية}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
 
-    let avone = await jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avone.circle()
-    let avtwo = await jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)
-    avtwo.circle()
-    let pth = "abcd.png"
-    let img = await jimp.read("https://i.imgur.com/wWgAzsU.jpg")
+    صورةالضارب.circle();
+    صورةالضحية.circle();
 
-    img.resize(432, 280).composite(avone.resize(60, 60), 200, 23).composite(avtwo.resize(60, 60), 136, 40);
+    // تحميل الصورة الأساسية من المسار المحلي
+    const الخلفية = await jimp.read("tmp/ضرب.png");
 
-    await img.writeAsync(pth)
-    return pth
+    // إضافة الوجوه في أماكنهم (تقديرية حسب الصورة اللي أرسلتها)
+    الخلفية
+        .composite(صورةالضحية.resize(80, 80), 85, 105)   // يسار – الضحية
+        .composite(صورةالضارب.resize(80, 80), 380, 45);  // يمين – الضارب
+
+    const الناتج = "tmp/output.png";
+    await الخلفية.writeAsync(nاتج);
+    return الناتج;
 }
