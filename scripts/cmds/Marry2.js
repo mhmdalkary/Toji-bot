8ac5874af09440b2490a04213c09925773680ba0
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-08 21:03:30 +0200

    Update Marry2.js

--- scripts/cmds/Marry2.js
+++ scripts/cmds/Marry2.js
@@ -1,6 +1,7 @@
-const axios = require('axios');
-const Jimp = require('jimp');
-const fs = require('fs');
+const { createCanvas, loadImage } = require("canvas");
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
 module.exports = {
   config: {
@@ -16,56 +17,78 @@ module.exports = {
     guide: "{pn}"
   },
 
-  onStart: async function({ message, event, args }) {
+  onStart: async function({ api, event }) {
     const mention = Object.keys(event.mentions);
+    if (mention.length === 0)
+      return api.sendMessage("أرجوك قم بعمل منشن للشخص الذي تريد الزواج به 🙃", event.threadID);
 
-    if (mention.length === 0) {
-      return message.reply("أرجوك قم بعمل منشن للشخص الذي تريد الزواج به 🙃");
-    } else if (mention.length === 1) {
-      const one = event.senderID;
-      const two = mention[0];
-      bal(one, two).then(path => {
-        message.reply({ body: "「 أحبكي عزيزتي 🥰❤️ 」", attachment: fs.createReadStream(path) });
-      }).catch(err => {
-        console.error(err);
-        message.reply("حدث خطأ أثناء تجهيز الصورة.");
-      });
-    } else {
-      const one = mention[1];
-      const two = mention[0];
-      bal(one, two).then(path => {
-        message.reply({ body: "「 أحبكي عزيزتي 🥰❤️ 」", attachment: fs.createReadStream(path) });
-      }).catch(err => {
-        console.error(err);
-        message.reply("حدث خطأ أثناء تجهيز الصورة.");
-      });
+    const one = mention.length === 1 ? event.senderID : mention[1];
+    const two = mention.length === 1 ? mention[0] : mention[0];
+
+    try {
+      const outputPath = await bal(one, two);
+      api.sendMessage(
+        {
+          body: "「 أحبكي عزيزتي 🥰❤️ 」",
+          attachment: fs.createReadStream(outputPath)
+        },
+        event.threadID,
+        () => fs.unlink(outputPath, () => {})
+      );
+    } catch (err) {
+      console.error(err);
+      api.sendMessage("حدث خطأ أثناء تجهيز الصورة.", event.threadID);
     }
   }
 };
 
 async function bal(one, two) {
-  // رابط الصورة الأساسية
+  // رابط الصورة الأساسية الخلفية
   const templateURL = "https://i.postimg.cc/26f9zkTc/marry.png";
-  // رابط صور الأفاتار مع توكن (استبدل توكنك إذا لزم الأمر)
   const token = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
 
+  // روابط صور الفيسبوك
   const avoneURL = `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=${token}`;
   const avtwoURL = `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=${token}`;
 
-  // قراءة الصور
-  let avone = await Jimp.read(avoneURL);
-  avone.circle();
+  // تحميل الصور
+  const [bg, avoneImg, avtwoImg] = await Promise.all([
+    loadImage(templateURL),
+    loadImage(await fetchImageBuffer(avoneURL)),
+    loadImage(await fetchImageBuffer(avtwoURL))
+  ]);
 
-  let avtwo = await Jimp.read(avtwoURL);
-  avtwo.circle();
+  // إنشاء كانفاس
+  const canvas = createCanvas(bg.width, bg.height);
+  const ctx = canvas.getContext("2d");
 
-  let img = await Jimp.read(templateURL);
-  img.resize(432, 280)
-    .composite(avone.resize(60, 60), 189, 15)
-    .composite(avtwo.resize(60, 60), 122, 25);
+  // رسم الخلفية
+  ctx.drawImage(bg, 0, 0);
 
-  const outputPath = "./marry.png";
-  await img.writeAsync(outputPath);
+  // دالة لرسم صورة دائرية
+  function drawCircleImage(img, x, y, size) {
+    ctx.save();
+    ctx.beginPath();
+    ctx.arc(x + size / 2, y + size / 2, size / 2, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.clip();
+    ctx.drawImage(img, x, y, size, size);
+    ctx.restore();
+  }
+
+  // رسم صور الأفاتار دائريًا مع الحجم والإحداثيات حسب تصميمك
+  drawCircleImage(avoneImg, 189, 15, 60);
+  drawCircleImage(avtwoImg, 122, 25, 60);
+
+  // حفظ الصورة محليًا
+  const outputPath = path.join(__dirname, "marry.png");
+  await fs.writeFile(outputPath, canvas.toBuffer("image/png"));
 
   return outputPath;
+}
+
+// دالة لتحويل رابط الصورة إلى بافر
+async function fetchImageBuffer(url) {
+  const response = await axios.get(url, { responseType: "arraybuffer" });
+  return response.data;
 }
