def4ddcab8aa5b3193b09a0a0c2032ae2e7064fa
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-08 20:41:33 +0200

    Update Marry2.js

--- scripts/cmds/Marry2.js
+++ scripts/cmds/Marry2.js
@@ -1,51 +1,71 @@
 const axios = require('axios');
-const Jimp = require("jimp");  // غيرت jimp إلى Jimp
-const fs = require("fs");
+const Jimp = require('jimp');
+const fs = require('fs');
 
 module.exports = {
-    config: {
-        name: "زوجني2",
-        aliases: ["زواج", "waifu"],  // صححت الفاصلة في aliases
-        version: "1.0",
-        author: "AceGun",
-        countDown: 5,
-        role: 0,
-        shortDescription: "قم بالحصول على زوجة",
-        longDescription: "",
-        category: "حب",
-        guide: "{pn}"
-    },
+  config: {
+    name: "زوجني2",
+    aliases: ["زواج", "waifu"],
+    version: "1.0",
+    author: "AceGun",
+    countDown: 5,
+    role: 0,
+    shortDescription: "قم بالحصول على زوجة",
+    longDescription: "",
+    category: "حب",
+    guide: "{pn}"
+  },
 
-    onStart: async function ({ message, event, args }) {
-        const mention = Object.keys(event.mentions);
-        if(mention.length == 0) 
-            return message.reply("أرجوك فم بعمل منشن للشخص اللذي تريد الزواج به 🙃");
-        else if(mention.length == 1){
-            const one = event.senderID, two = mention[0];
-            bal(one, two).then(ptth => { 
-                message.reply({ body: "「 أحبكي عزيزتي 🥰❤️ 」", attachment: fs.createReadStream(ptth) }) 
-            });
-        } else {
-            const one = mention[1], two = mention[0];
-            bal(one, two).then(ptth => { 
-                message.reply({ body: "「 أحبكي عزيزتي🥰❤️ 」", attachment: fs.createReadStream(ptth) }) 
-            });
-        }
+  onStart: async function({ message, event, args }) {
+    const mention = Object.keys(event.mentions);
+
+    if (mention.length === 0) {
+      return message.reply("أرجوك قم بعمل منشن للشخص الذي تريد الزواج به 🙃");
+    } else if (mention.length === 1) {
+      const one = event.senderID;
+      const two = mention[0];
+      bal(one, two).then(path => {
+        message.reply({ body: "「 أحبكي عزيزتي 🥰❤️ 」", attachment: fs.createReadStream(path) });
+      }).catch(err => {
+        console.error(err);
+        message.reply("حدث خطأ أثناء تجهيز الصورة.");
+      });
+    } else {
+      const one = mention[1];
+      const two = mention[0];
+      bal(one, two).then(path => {
+        message.reply({ body: "「 أحبكي عزيزتي 🥰❤️ 」", attachment: fs.createReadStream(path) });
+      }).catch(err => {
+        console.error(err);
+        message.reply("حدث خطأ أثناء تجهيز الصورة.");
+      });
     }
+  }
 };
 
 async function bal(one, two) {
-    let avone = await Jimp.read(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-    avone.circle();
-    let avtwo = await Jimp.read(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`);
-    avtwo.circle();
-    let pth = "marry.png";
-    let img = await Jimp.read("https://i.postimg.cc/26f9zkTc/marry.png");
+  // رابط الصورة الأساسية
+  const templateURL = "https://i.postimg.cc/26f9zkTc/marry.png";
+  // رابط صور الأفاتار مع توكن (استبدل توكنك إذا لزم الأمر)
+  const token = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
 
-    img.resize(432, 280)
-       .composite(avone.resize(60, 60), 189, 15)
-       .composite(avtwo.resize(60, 60), 122, 25);
+  const avoneURL = `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=${token}`;
+  const avtwoURL = `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=${token}`;
 
-    await img.writeAsync(pth);
-    return pth;
+  // قراءة الصور
+  let avone = await Jimp.read(avoneURL);
+  avone.circle();
+
+  let avtwo = await Jimp.read(avtwoURL);
+  avtwo.circle();
+
+  let img = await Jimp.read(templateURL);
+  img.resize(432, 280)
+    .composite(avone.resize(60, 60), 189, 15)
+    .composite(avtwo.resize(60, 60), 122, 25);
+
+  const outputPath = "./marry.png";
+  await img.writeAsync(outputPath);
+
+  return outputPath;
 }
