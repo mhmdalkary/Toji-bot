4351f2e3822413e78b2ebed93390a484bc2a1750
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-08 19:33:23 +0200

    Update Rip.js

--- scripts/cmds/Rip.js
+++ scripts/cmds/Rip.js
@@ -1,59 +1,31 @@
-const fs = require('fs');
-const path = require('path');
-const axios = require('axios');
-const jimp = require('jimp');
-const { createCanvas, loadImage } = require('canvas');
+const DIG = require("discord-image-generation");
+const fs = require("fs-extra");
 
 module.exports = {
-    config: {
-        name: "قبر",
-        version: "1.0",
-        author: "Mahim",
-        role: 0,
-        countdown: 10,
-        category: "ميمز وتعديل الصور",
-        shortDescription: {
-            ar: " "
-        },
-        longDescription: {
-            ar: ""
-        },
-        guide: {
-            ar: "{prefix}"
-        }
-    },
-
-    circle: async (image) => {
-        image = await jimp.read(image);
-        image.circle();
-        return await image.getBufferAsync("image/png");
-    },
-
-    onStart: async function ({ event, api }) {
-        try {
-            const canvas = createCanvas(500, 670);
-            const ctx = canvas.getContext('2d');
-            const background = await loadImage('https://i.imgur.com/A4quyh3.jpg');
-
-            const { threadID, messageID } = event;
-            const mention = Object.keys(event.mentions);
-            if (!mention[0]) return api.sendMessage(" ⚠️ | إعمل تاغ يا بني", threadID, messageID);
-            else {
-                const one = event.senderID, two = mention[0];
-                const avatar = await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' });
-                const circleOne = await module.exports.circle(avatar.data);
-
-                ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-                ctx.drawImage(await loadImage(circleOne), 160, 70, 160, 160);
-
-                const imageBuffer = canvas.toBuffer();
-                const imagePath = path.join(__dirname, 'cache', 'canvas', `sato${one}_${two}.png`);
-                fs.writeFileSync(imagePath, imageBuffer);
-
-                api.sendMessage({ body: "الله يرحمو 🤲\nكان إنسانا طيبا 🥺", attachment: fs.createReadStream(imagePath) }, threadID, () => fs.unlinkSync(imagePath), messageID);
-            }
-        } catch (e) {
-            api.sendMessage(e.stack, event.threadID);
-        }
+  config: {
+    name: "قبر",
+    version: "1.1",
+    author: "MILAN",
+    countDown: 5,
+    role: 0,
+    shortDescription: "صورة على قبر",
+    longDescription: "صورة صديق على قبر من اجل المتعة ليس إلا 😂",
+    category: "متعة",
+    guide: {
+      vi: "{pn} [@tag | blank]",
+      ar: "{pn} [@منشن]"
     }
+  },
+
+  onStart: async function ({ event, message, usersData }) {
+ const uid = Object.keys(event.mentions)[0]
+ if(!uid) return message.reply(" ⚠️ | المرجو عمل @منشن لشخص ما")
+    const avatarURL = await usersData.getAvatarUrl(uid);
+    const img = await new DIG.Rip().getImage(avatarURL);
+ const pathSave = `${__dirname}/tmp/${uid}_Rip.png`;
+    fs.writeFileSync(pathSave, Buffer.from(img));
+    message.reply({
+      attachment: fs.createReadStream(pathSave)
+    }, () => fs.unlinkSync(pathSave));
+  }
 };
