da16a101780c6aade30aeaa1306881f009659f3a
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-09 10:28:21 +0200

    Create MyAge.js

---
+++ scripts/cmds/MyAge.js
@@ -0,0 +1,31 @@
+module.exports = {
+  config: {
+    name: "عمري",
+    author: "Samir Œ",
+    countDown: 5,
+    role: 0,
+    category: "خدمات",
+    shortDescription: {
+      ar: "منشن صديقك واكتب شيء لنشره✍️",
+    },
+  },
+
+  onStart: async function ({ api, event, args }) {
+    const birthday = args[0];
+
+    if (!birthday) {
+      return api.sendMessage(" ⚠️ |يرجى إدخال تاريخ ميلادك مثل هذه الصيغة السنة-الشهر-اليوم .", event.threadID);
+    }
+
+    const currentDate = new Date();
+    const birthDate = new Date(birthday);
+    const age = currentDate.getFullYear() - birthDate.getFullYear();
+
+    birthDate.setFullYear(currentDate.getFullYear());
+    const isBeforeBirthday = currentDate < birthDate;
+
+    const finalAge = isBeforeBirthday ? age - 1 : age;
+
+    api.sendMessage(` ✅ | عمرك هو ${finalAge}\nهل أنا على صواب `, event.threadID);
+  },
+};
