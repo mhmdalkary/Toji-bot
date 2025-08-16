84fe0bb777377a1c6d759d5654da82672b0dae86
mhmdalkary <hmadyy.2009@gmail.com>
2025-08-16 08:07:27 +0200

    Update Aatqal.js

--- scripts/cmds/Aatqal.js
+++ scripts/cmds/Aatqal.js
@@ -7,26 +7,31 @@ module.exports = {
   config: {
     name: "اعتقال",
     aliases: ["arrest"],
-    version: "1.0",
-    author: "milan-says",
+    version: "1.1",
+    author: "milan-says | تعديل محمد",
     countDown: 5,
     role: 0,
     shortDescription: "قم بإعقال من خرق القوانين",
-    longDescription: "إعتقال أحد ما انطلاقا من وضعك للمنشن عليه",
+    longDescription: "إعتقال أحد ما انطلاقا من وضعك للتاغ أو الرد عليه",
     category: "ميمز وتعديل الصور",
-    guide: {
-      vi: "{pn} [@tag]",
-      ar: "{pn} [@تاغ]"
-    }
+    guide: "{pn} [@تاغ] | الرد على رسالة"
   },
 
   onStart: async function({ api, event }) {
     const mention = Object.keys(event.mentions);
-    if (mention.length === 0) 
-      return api.sendMessage("أرجوك قم بعمل منشن للشخص الذي تريد إعتقاله", event.threadID);
+    let one = event.senderID, two;
 
-    const one = mention.length === 1 ? event.senderID : mention[1];
-    const two = mention.length === 1 ? mention[0] : mention[0];
+    // التاغ
+    if (mention.length > 0) {
+      two = mention[0];
+    }
+    // الرد على رسالة
+    else if (event.type === "message_reply") {
+      two = event.messageReply.senderID;
+    }
+    else {
+      return api.sendMessage("أرجوك قم بعمل منشن أو رد على رسالة الشخص الذي تريد إعتقاله", event.threadID, event.messageID);
+    }
 
     try {
       const outputPath = await bal(one, two);
@@ -36,38 +41,34 @@ module.exports = {
           attachment: fs.createReadStream(outputPath)
         },
         event.threadID,
-        () => fs.unlink(outputPath, () => {})
+        () => fs.unlink(outputPath, () => {}),
+        event.messageID
       );
     } catch (err) {
       console.error(err);
-      api.sendMessage("حدث خطأ أثناء تجهيز الصورة.", event.threadID);
+      api.sendMessage("حدث خطأ أثناء تجهيز الصورة.", event.threadID, event.messageID);
     }
   }
 };
 
 async function bal(one, two) {
-  // رابط الخلفية
   const templateURL = "https://i.imgur.com/ep1gG3r.png";
   const token = "6628568379%7Cc1e620fa708a1d5696fb991c1bde5662";
 
   const avoneURL = `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=${token}`;
   const avtwoURL = `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=${token}`;
 
-  // تحميل الصور
   const [bg, avoneImg, avtwoImg] = await Promise.all([
     loadImage(templateURL),
     loadImage(await fetchImageBuffer(avoneURL)),
     loadImage(await fetchImageBuffer(avtwoURL))
   ]);
 
-  // إنشاء الكانفاس
   const canvas = createCanvas(bg.width, bg.height);
   const ctx = canvas.getContext("2d");
 
-  // رسم الخلفية
   ctx.drawImage(bg, 0, 0);
 
-  // دالة لرسم صورة دائرية
   function drawCircleImage(img, x, y, size) {
     ctx.save();
     ctx.beginPath();
@@ -78,18 +79,15 @@ async function bal(one, two) {
     ctx.restore();
   }
 
-  // رسم الصور الدائرية مع إحداثيات وأحجام متناسبة مع التصميم
   drawCircleImage(avoneImg, 375, 9, 100);
   drawCircleImage(avtwoImg, 160, 92, 100);
 
-  // حفظ الصورة محليًا
   const outputPath = path.join(__dirname, "fak.png");
   await fs.writeFile(outputPath, canvas.toBuffer("image/png"));
 
   return outputPath;
 }
 
-// دالة لتحويل رابط الصورة إلى بافر
 async function fetchImageBuffer(url) {
   const response = await axios.get(url, { responseType: "arraybuffer" });
   return response.data;
